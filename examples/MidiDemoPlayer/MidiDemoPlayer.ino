4b4bae1786150b8a85784949a395beb6ce7e2a12
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 11:35:31 +0800

    Pretty printed the Arduino code with astyle

--- examples/MidiDemoPlayer/MidiDemoPlayer.ino
+++ examples/MidiDemoPlayer/MidiDemoPlayer.ino
@@ -3,13 +3,13 @@
 #include <arduino.h>
 #include <MusicPlayer.h>
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  player.beginInMidiFmt();
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    player.beginInMidiFmt();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  player.midiDemoPlayer();
-  delay(500);
+    // put your main code here, to run repeatedly:
+    player.midiDemoPlayer();
+    delay(500);
 }
